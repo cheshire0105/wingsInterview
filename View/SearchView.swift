c7ab3c0c90a77bb69cda5aa4641ba03b19ef610a
cheshire0105 <cheshire0105@gmail.com>
2024-01-09 21:15:39 +0900

    [서치 페이지]

---
+++ View/SearchView.swift
@@ -0,0 +1,23 @@
+//
+//  SearchView.swift
+//  wingsInterview
+//
+//  Created by cheshire on 1/9/24.
+//
+
+import Foundation
+import SwiftUI
+
+// 검색 페이지를 위한 뷰
+struct SearchView: View {
+
+    var body: some View {
+        ZStack {
+            // 배경 색상 설정
+            Color("BackColor") // Assets에서 정의된 배경색 사용
+                .edgesIgnoringSafeArea(.all)
+
+
+        }
+    }
+}
